9c48ad3aa1d9c805e57aaf08987776fca3d40281
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-10-06 20:40:39 -0500

    added sql setup

--- migration.sql
+++ migration.sql
@@ -1,4 +1,4 @@
-CRECREATE DATABASE IF NOT EXISTS blog_db;
-
-CRECREATE USER blog_user@localhost IDENTIFIED BY 'codeup';
-GRANT ALL ON blog_db.* TO blog_user@localhost;
+-- CREATE DATABASE IF NOT EXISTS blog_db;
+--
+-- CREATE USER 'blog_user'@'localhost' IDENTIFIED BY 'codeup';
+-- GRANT ALL ON blog_db.* TO blog_user@localhost;
---
+++ seeder.sql
@@ -0,0 +1,5 @@
+USE blog_db;
+
+INSERT INTO posts (title, description, body) VALUES
+('Raising the Bar', 'Up, up and away… our new Poolside Provisions vintage truck took to the air, soaring (carefully) three stories to be installed beside the rooftop pool.', 'It’s the culmination of an 18-month design and renovation feat that’s given a 1960s-era Pearl beer truck new life as a poolside bar, geared up to satisfy our guests’ al fresco cravings for food and drink, once it’s opened for business in March.')
+;
