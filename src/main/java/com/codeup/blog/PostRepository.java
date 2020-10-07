6059189fc268c902f2f5ae23404c55df9a4daac6
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-10-07 10:55:09 -0500

    all i got

--- src/main/java/com/codeup/blog/PostRepository.java
+++ src/main/java/com/codeup/blog/PostRepository.java
@@ -7,12 +7,4 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-////p = alias for table name
-//    @Query("from Post p where p.id like ?1")
-//    Post getPostById(long id);
-//
-//    // The following method shows you how to use named parameters in a HQL custom query:
-//    @Query("from Post p where p.title like %:term%")
-//    List<Post> searchByTitleLike(@Param("term") String term);
-
 }
